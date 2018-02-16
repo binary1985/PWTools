c68490d99ecce08f73eef3e22a6213b3974f4001
binary1985 <joshua.alan.platz@gmail.com>
2018-02-16 09:17:12 -0600

    Create xmute.sh

---
+++ language-xmute/xmute.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+while read p;
+do echo $p;
+p1=$p;p1+="1";echo $p1;
+p1=$p;p1+='1!';echo $p1;
+p1=$p;p1+="123";echo $p1;
+p1=$p;p1+='123!';echo $p1;
+echo $p|sed 's/.*/\u&/';
+p1=$p;p1+="1";echo $p1|sed 's/.*/\u&/';
+p1=$p;p1+='1!';echo $p1|sed 's/.*/\u&/';
+p1=$p;p1+="123";echo $p1|sed 's/.*/\u&/';
+p1=$p;p1+='123!';echo $p1|sed 's/.*/\u&/';
+p1=$p;p1+="2015";echo $p1;
+p1=$p;p1+="2016";echo $p1;
+p1=$p;p1+="2017";echo $p1;
+p1=$p;p1+="2018";echo $p1;
+p1=$p;p1+="2015!";echo $p1;
+p1=$p;p1+="2016!";echo $p1;
+p1=$p;p1+="2017!";echo $p1;
+p1=$p;p1+="2018!";echo $p1;
+done<$1>$1-xmute.txt
+sort -u $1-xmute.txt > $1-xmute.txt.tmp
+mv $1-xmute.txt.tmp $1-xmute.txt
