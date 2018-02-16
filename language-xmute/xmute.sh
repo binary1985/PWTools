26e8c6511cf4b5e67d6c69b71096aab04f77ec69
binary1985 <joshua.alan.platz@gmail.com>
2018-02-16 09:47:45 -0600

    Update xmute.sh

--- language-xmute/xmute.sh
+++ language-xmute/xmute.sh
@@ -18,6 +18,14 @@ p1=$p;p1+="2015!";echo $p1;
 p1=$p;p1+="2016!";echo $p1;
 p1=$p;p1+="2017!";echo $p1;
 p1=$p;p1+="2018!";echo $p1;
+p1=$p;p1+="2015";echo $p1|sed 's/.*/\u&/';
+p1=$p;p1+="2016";echo $p1|sed 's/.*/\u&/';
+p1=$p;p1+="2017";echo $p1|sed 's/.*/\u&/';
+p1=$p;p1+="2018";echo $p1|sed 's/.*/\u&/';
+p1=$p;p1+="2015!";echo $p1|sed 's/.*/\u&/';
+p1=$p;p1+="2016!";echo $p1|sed 's/.*/\u&/';
+p1=$p;p1+="2017!";echo $p1|sed 's/.*/\u&/';
+p1=$p;p1+="2018!";echo $p1|sed 's/.*/\u&/';
 done<$1>$1-xmute.txt
 sort -u $1-xmute.txt > $1-xmute.txt.tmp
 mv $1-xmute.txt.tmp $1-xmute.txt
