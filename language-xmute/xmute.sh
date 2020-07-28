f0c572f2f8683768ae88c4831265400a028adf63
binary1985 <joshua.alan.platz@gmail.com>
2020-07-28 11:52:08 -0500

    Update xmute.sh

--- language-xmute/xmute.sh
+++ language-xmute/xmute.sh
@@ -14,18 +14,26 @@ p1=$p;p1+="2015";echo $p1;
 p1=$p;p1+="2016";echo $p1;
 p1=$p;p1+="2017";echo $p1;
 p1=$p;p1+="2018";echo $p1;
+p1=$p;p1+="2019";echo $p1;
+p1=$p;p1+="2020";echo $p1;
 p1=$p;p1+="2015!";echo $p1;
 p1=$p;p1+="2016!";echo $p1;
 p1=$p;p1+="2017!";echo $p1;
 p1=$p;p1+="2018!";echo $p1;
+p1=$p;p1+="2019!";echo $p1;
+p1=$p;p1+="2020!";echo $p1;
 p1=$p;p1+="2015";echo $p1|sed 's/.*/\u&/';
 p1=$p;p1+="2016";echo $p1|sed 's/.*/\u&/';
 p1=$p;p1+="2017";echo $p1|sed 's/.*/\u&/';
 p1=$p;p1+="2018";echo $p1|sed 's/.*/\u&/';
+p1=$p;p1+="2019";echo $p1|sed 's/.*/\u&/';
+p1=$p;p1+="2020";echo $p1|sed 's/.*/\u&/';
 p1=$p;p1+="2015!";echo $p1|sed 's/.*/\u&/';
 p1=$p;p1+="2016!";echo $p1|sed 's/.*/\u&/';
 p1=$p;p1+="2017!";echo $p1|sed 's/.*/\u&/';
 p1=$p;p1+="2018!";echo $p1|sed 's/.*/\u&/';
+p1=$p;p1+="2019!";echo $p1|sed 's/.*/\u&/';
+p1=$p;p1+="2020!";echo $p1|sed 's/.*/\u&/';
 done<$1>$1-xmute.txt
 sort -u $1-xmute.txt > $1-xmute.txt.tmp
 mv $1-xmute.txt.tmp $1-xmute.txt
